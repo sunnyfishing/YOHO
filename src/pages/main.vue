3aede0182aaf352e5556b01a971c78c543ec8fd1
17839221839 <17839221839@163.com>
2017-10-23 14:18:50 +0800

    迁移完善项目-teng-10.23

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,5 +1,17 @@
 <template lang="html">
-	
+	<div class="main">
+		<div class="search">
+			<div class="con">
+				<i class="yo-ico">&#xe61c;</i>
+				<div class="input">
+					<input type="text" />
+					<i class="yo-ico">&#xe610;</i>
+					<i class="yo-ico">&#xe649;</i>
+				</div>
+				<i class="yo-ico">&#xe601;</i>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
