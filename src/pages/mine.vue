323eefd11b251f413857001fe0c90c03d876aa73
1809642355 <1809642355@qq.com>
2017-10-24 08:38:45 +0800

    第一次commit

--- src/pages/mine.vue
+++ src/pages/mine.vue
@@ -1,4 +1,119 @@
 <template lang="html">
+	<div id="mine">
+		<!--<i class="yo-ico">&#xe61c;</i>-->
+		<div class="mine-head">
+			<div class="mine-tit">
+				<i class="yo-ico">&#xe502;</i>
+				<span>我的</span>
+				<i class="yo-ico">&#xe61c;</i>
+			</div>
+			<div class="mine-login">
+				<button>登录/注册</button>
+			</div>
+			<div class="mine-items">
+				<ul>
+					<li>收藏的商品</li>
+					<i></i>
+					<li>收藏的品牌</li>
+					<i></i>
+					<li>浏览的记录</li>
+				</ul>
+			</div>
+		</div>
+		<div class="mine-cannel">
+			<p>默认购物频道</p>
+			<div class="mine-can-item">
+				<span>男士MEN</span>
+				<i class='yo-ico'>&#xe60d;</i>
+			</div>
+		</div>
+		<div class="mine-order">
+			<div class="mine-order-tit">
+				<p>我的订单</p>
+				<div class="mine-order-all">
+					<span>查看全部订单</span>
+					<i class="yo-ico">&#xe60d;</i>
+				</div>
+			</div>
+			<div class="mine-order-items">
+				<ul>
+					<li>
+						<i class="yo-ico"></i>
+						<span>待付款</span>
+					</li>
+					<li>
+						<i class="yo-ico"></i>
+						<sapn>待发货</sapn>
+					</li>
+					<li>
+						<i class="yo-ico"></i>
+						<span>待收货</span>
+					</li>
+					<li>
+						<i class="yo-ico"></i>
+						<span>待晒单</span>
+					</li>
+					<li>
+						<i class="yo-ico"></i>
+						<span>退/换货</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="mine-order">
+			<div class="mine-order-tit">
+				<p>我的资产</p>
+			</div>
+			<div class="mine-order-items">
+				<ul>
+					<li>
+						<i class="yo-ico"></i>
+						<span>优惠券</span>
+					</li>
+					<li>
+						<i class="yo-ico"></i>
+						<sapn>有货币</sapn>
+					</li>
+					<li>
+						<i class="yo-ico"></i>
+						<span>限购码</span>
+					</li>
+					<li>
+						<i class="yo-ico"></i>
+						<span>有货分期</span>
+					</li>
+					<li>
+						<i class="yo-ico"></i>
+						<span>礼品卡</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="mine-buy">
+			<div class="mine-buy-shopping">
+				<i class="yo-ico">1sfsd</i>
+				<span>我的逛</span>
+				<i class="yo-ico">1</i>
+			</div>
+			<div class="mine-buy-shopping">
+				<i class="yo-ico">a</i>
+				<span>我的晒单</span>
+				<i class="yo-ico">e</i>
+			</div>
+		</div>
+		<div class="mine-buy">
+			<div class="mine-buy-shopping">
+				<i class="yo-ico">1sfsd</i>
+				<span>邀请好友,50元现金券无限赚</span>
+				<i class="yo-ico">1</i>
+			</div>
+			<div class="mine-buy-shopping">
+				<i class="yo-ico">a</i>
+				<span>分享购</span>
+				<i class="yo-ico">e</i>
+			</div>
+		</div>
+	</div>
 	
 </template>
 
